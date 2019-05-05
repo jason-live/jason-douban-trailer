d1a5023f88edbb4cd6149f3d4c67523e04ca96f1
袁杨 <yuanyang@uvoplus.com>
2019-05-05 22:52:40 +0800

    6-6

---
+++ server/tasks/trailer.js
@@ -0,0 +1,35 @@
+const cp = require('child_process');
+
+const { resolve } = require('path');
+
+; (async () => {
+  const script = resolve(__dirname, '../crawler/video')
+
+  const child = cp.fork(script, []);
+
+  let invoke = false;
+
+  child.on('error', err => {
+    if (invoke) {
+      return;
+    }
+    invoke = true;
+    console.log(err);
+  });
+
+  child.on('exit', code => {
+    if (invoke) {
+      return;
+    }
+    invoke = true;
+    let err = code === 0 ? null : new Error('exit code' + code);
+
+    console.log(err);
+  });
+
+  child.on('message', data => {
+    let result = data;
+    console.log(data);
+  });
+
+})();
