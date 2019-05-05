fd0dedbcc441ff9243f316512adbaec79c829007
袁杨 <yuanyang@uvoplus.com>
2019-05-06 00:16:22 +0800

    6-7

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "qiniu": {
+    "bucket": "jason-douban",
+    "video": "http://pqgzybcoi.bkt.clouddn.com/",
+    "AK": "EsIjttda-p7fiuwQOLFswcqbWCebDPUwOtA5IzOZ",
+    "SK": "Jl7CfBcugIoKgqdSpOFkiTtQ9eKP_kJIOe0gcJL8"
+  }
+}
