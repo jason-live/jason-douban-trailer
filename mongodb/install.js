c9b6bb7db06017a34096820bf8e3e65582d36aeb
袁杨 <yuanyang@uvoplus.com>
2019-05-06 23:24:58 +0800

    8-1

---
+++ mongodb/install.js
@@ -0,0 +1,9 @@
+// 官网 https://docs.mongodb.com/manual/administration/install-community/
+
+// brew install mongodb
+
+// brew services start mongodb
+
+// mongo --host 127.0.0.1:27017
+
+// show dbs;
