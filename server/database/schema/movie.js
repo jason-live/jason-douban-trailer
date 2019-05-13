06fd35e5ea9778a99c374e21edd66e2adf13cf63
袁杨 <yuanyang@uvoplus.com>
2019-05-13 23:40:08 +0800

    daily-8-4

--- server/database/schema/movie.js
+++ server/database/schema/movie.js
@@ -1,3 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Mixed = Schema.Types.Mixed;
+
+const movieSchema = new Schema({
+  doubanId: String,
+  rate: Number,
+  title: String,
+  summary: String,
+  video: String,
+  poster: String,
+  cover: String,
+
+  videoKey: String,
+  posterKey: String,
+  coverKey: String,
+
+  rawTitle: String,
+  movieTypes: [String],
+  pubdate: Mixed,
+  year: Number,
+
+  tags: [String],
+
+  meta: {
+    createAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    updateAt: {
+      type: Date,
+      default: Date.now(),
+    }
+  }
+});
+
+mongoose.model('Movie', movieSchema);
