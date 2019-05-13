054049a626028b9b7d594e3c4eb9d76eefa14e52
袁杨 <yuanyang@uvoplus.com>
2019-05-13 22:44:15 +0800

    daily-8-2

---
+++ server/database/init.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://localhost/douban-test';
+mongoose.Promise = global.Promise;
+
+exports.connect = () => {
+  let maxConnectCount = 0;
+
+  return new Promise((resolve, reject) => {
+    if (process.env.NODE_ENV !== 'production') {
+      mongoose.set('debug', true);
+    };
+
+    mongoose.connect(db);
+
+    mongoose.connection.on('disconnected', () => {
+      maxConnectCount ++;
+      if (maxConnectCount < 5) {
+        mongoose.connect(db);
+      } else {
+        throw new Error('数据库挂了吧！')
+      };
+    });
+
+    mongoose.connection.on('error', (err) => {
+      maxConnectCount++;
+      if (maxConnectCount < 5) {
+        mongoose.connect(db);
+      } else {
+        throw new Error('数据库挂了吧！')
+      }
+    });
+
+    mongoose.connection.on('open', () => {
+      const Dog = mongoose.model('Dog', { name: String });
+      const dog = new Dog({ name: '阿尔法' });
+      dog.save().then(() => {
+        console.log('wang');
+      });
+
+      resolve();
+      console.log('MongoBD connected success!');
+    });
+  });
+};
