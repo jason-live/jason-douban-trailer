8d26c22f4b97b50b5ae9b7eadca98724c6bfd8ab
袁杨 <yuanyang@uvoplus.com>
2019-05-19 23:02:17 +0800

    daily-10-2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './asset/common.sass';
 
 function changeTitle() {
   setTimeout(function (params) {
-    window.$('#app').html('parcel 大包包');
+    window.$('#app').html('parcel 大包包1');
   }, 2000)
 }
 
